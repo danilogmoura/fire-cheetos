893abd5621d35a49f73bff49c3fa35a39cf7419c
Danilo <danilogmoura@gmail.com>
2025-02-19 03:25:41 -0300

    Update EnemySpawner.cs

--- Hooks/EnemySpawner.cs
+++ Hooks/EnemySpawner.cs
@@ -11,7 +11,12 @@ namespace FireCheetos.Hooks
             if (!Main.feature3Enabled) return; // Executa o código original se a opção estiver desativada
 
             // Log para depuração
-            foreach (var instanceBattleEnemy in __instance.battleEnemies) instanceBattleEnemy.currentHealth = 1;
+            foreach (var instanceBattleEnemy in __instance.battleEnemies)
+            {
+                instanceBattleEnemy.currentHealth = 1;
+                instanceBattleEnemy.damageTotal = 1;
+                instanceBattleEnemy.armorTotal = 1;
+            }
         }
     }
 }
