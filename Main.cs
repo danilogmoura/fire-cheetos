7d3b490e2433172d8ab8c47e6da8a081a85d5548
Danilo <danilogmoura@gmail.com>
2025-02-21 02:45:57 -0300

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -16,7 +16,6 @@ namespace FireCheetos
         public const string Author = "danilogmoura";
         public const string Company = null;
         public const string Version = "1.0.0";
-        public const string DownloadLink = null;
     }
 
     public class Main : MelonMod
@@ -26,13 +25,14 @@ namespace FireCheetos
         public static bool feature3Enabled;
         public static bool feature4Enabled;
         public static bool feature5Enabled;
+        public static bool feature6Enabled;
 
         private List<Button> buttonsUpgrade;
-        private object clickCoroutine; // Armazena a corrotina em execução
+        private object clickCoroutine;
 
-        private bool showMenu; // Variável para ativar/desativar o menu
+        private bool showMenu;
 
-        public override void OnInitializeMelon()
+        public override void OnLateInitializeMelon()
         {
             InjectionDetector.StopDetection();
             ObscuredCheatingDetector.StopDetection();
@@ -42,7 +42,7 @@ namespace FireCheetos
 
             var harmony = new HarmonyLib.Harmony("br.com.danilogmoura.ieh");
             harmony.PatchAll();
-            MelonLogger.Msg("Fire Cheetos Inicializado!");
+            MelonDebug.Msg("Fire Cheetos Initialized!");
         }
 
         public override void OnUpdate()
@@ -58,40 +58,40 @@ namespace FireCheetos
                 var button = buttonObject.GetComponent<Button>();
                 if (button != null)
                 {
-                    button.onClick.Invoke(); // Simula um clique no botão
-                    MelonDebug.Msg($"Botão {buttonName} pressionado!");
+                    button.onClick.Invoke();
+                    MelonDebug.Msg($"Button {buttonName} pressed!");
                 }
                 else
                 {
-                    MelonLogger.Error($"O objeto {buttonName} não tem um componente Button!");
+                    MelonDebug.Error($"The object {buttonName} does not have a Button component!");
                 }
             }
             else
             {
-                MelonLogger.Error($"Botão {buttonName} não encontrado!");
+                MelonDebug.Error($"Button {buttonName} not found!");
             }
         }
 
         private void StartClicking()
         {
             clickCoroutine = MelonCoroutines.Start(ClickLoop());
-            MelonDebug.Msg("Auto-click iniciado!");
+            MelonDebug.Msg("Auto-click started!");
         }
 
         private void StopClicking()
         {
             if (clickCoroutine != null) MelonCoroutines.Stop(clickCoroutine);
             buttonsUpgrade = null;
-            MelonDebug.Msg("Auto-click parado!");
+            MelonDebug.Msg("Auto-click stopped!");
         }
 
         private IEnumerator ClickLoop()
         {
             while (feature2Enabled)
             {
-                yield return new WaitForSeconds(0.5f); // Tempo entre cliques (ajuste conforme necessário)
+                yield return new WaitForSeconds(0.5f);
                 ClickAllUpgradeButtons();
-                yield return new WaitForSeconds(0.5f); // Tempo entre cliques (ajuste conforme necessário)
+                yield return new WaitForSeconds(0.5f);
             }
         }
 
@@ -100,7 +100,7 @@ namespace FireCheetos
             var root = GameObject.Find("menusRoot/menuCanvasParent/SafeArea/menuCanvas/menus");
             if (root == null)
             {
-                MelonDebug.Error("menusRoot não encontrado!");
+                MelonDebug.Error("menusRoot not found!");
                 return;
             }
 
@@ -115,7 +115,7 @@ namespace FireCheetos
                     {
                         buttonsUpgrade.Add(button);
                         button.onClick.Invoke(); // Simula um clique no botão
-                        MelonDebug.Msg($"Botão {button.gameObject.name} clicado!");
+                        MelonDebug.Msg($"Button {button.gameObject.name} clicked!");
                     }
             }
 
@@ -132,7 +132,7 @@ namespace FireCheetos
             }
             catch (Exception e)
             {
-                MelonDebug.Error("Erro ao desativar o SpeedHackDetector: " + e);
+                MelonDebug.Error("Error desabling the SpeedHackDetector: " + e);
             }
 
             feature5Enabled = !feature5Enabled;
@@ -147,7 +147,7 @@ namespace FireCheetos
             const float menuX = 180;
             const float menuY = 80;
             const float width = 200;
-            const float height = 190;
+            const float height = 220;
 
             const float buttonX = menuX + 10;
             const float buttonWidth = 180;
@@ -189,10 +189,17 @@ namespace FireCheetos
             }
 
             if (GUI.Button(new Rect(buttonX, menuY + 115, buttonWidth, buttonHeight),
+                    feature6Enabled ? "XP Booster: ON" : "XP Booster: OFF"))
+            {
+                feature6Enabled = !feature6Enabled;
+                MelonDebug.Msg($"XP Booster: {feature6Enabled}");
+            }
+
+            if (GUI.Button(new Rect(buttonX, menuY + 150, buttonWidth, buttonHeight),
                     feature5Enabled ? "Speed Hack: ON" : "Speed Hack: OFF"))
                 ToggleSpeedHack();
 
-            if (GUI.Button(new Rect(buttonX, menuY + 150, buttonWidth, buttonHeight),
+            if (GUI.Button(new Rect(buttonX, menuY + 185, buttonWidth, buttonHeight),
                     feature3Enabled ? "Weakened Enemies: ON" : "Weakened Enemies: OFF"))
             {
                 feature3Enabled = !feature3Enabled;
