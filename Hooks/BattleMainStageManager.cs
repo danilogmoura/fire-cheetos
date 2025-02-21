7821351f84fb88f58f34445f06e69e0d5ae38cd9
Danilo <danilogmoura@gmail.com>
2025-02-21 02:45:41 -0300

    Create BattleMainStageManager.cs

---
+++ Hooks/BattleMainStageManager.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using MelonLoader;
+
+namespace FireCheetos.Hooks
+{
+    [HarmonyPatch(typeof(BattleMainStageManager), "CalculateWaveGold")]
+    public static class CalculateWaveGoldPatch
+    {
+        public static bool Prefix(BattleMainStageManager __instance, ref int CurrentStageNumber, int CurrentWaveNumber)
+        {
+            MelonDebug.Msg("Interceptado: CalculateWaveGold => CurrentStageNumber: " + CurrentStageNumber +
+                           " | CurrentWaveNumber: " + CurrentWaveNumber);
+
+            if (!Main.feature6Enabled) return true;
+
+            CurrentStageNumber += 10;
+            return true;
+        }
+    }
+}
